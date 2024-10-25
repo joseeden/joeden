88852b59c1cfcf37befeb695698adffeda3259e6
joseeden <josemanuelitoeden@gmail.com>
2024-10-26 03:51:16 +0800

    Change baseurl and url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,8 +7,8 @@ const config: Config = {
   title: "Eden Jose",
   tagline: "Engineer by day, runner by night.",
   favicon: 'img/logo/fourth/favicon.ico',
-  url: 'https://github.com',
-  baseUrl: "/joeden/",
+  url: 'https://github.com/joeden/',
+  baseUrl: "/",
   organizationName: 'joseeden',     // Github organization/username
   projectName: 'joeden',            // Reponame
   deploymentBranch: "master",
