80aaf3dda2e3691de2743bc67ac217a58345e16e
joseeden <josemanuelitoeden@gmail.com>
2025-02-04 06:09:59 +0800

    Update notes

--- docs/021-Software-Engineering/030-Javascript/Javascript-Course/01-Fundamentals/script.js
+++ docs/021-Software-Engineering/030-Javascript/Javascript-Course/01-Fundamentals/script.js
@@ -311,9 +311,9 @@
 
 
 // Practice 4 
-const bill = 430
+// const bill = 430
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20; 
-const total = bill + tip;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20; 
+// const total = bill + tip;
 
-console.log(`Bill: ${bill}\nTip: ${tip}\nTotal: ${total}`)
+// console.log(`Bill: ${bill}\nTip: ${tip}\nTotal: ${total}`)
