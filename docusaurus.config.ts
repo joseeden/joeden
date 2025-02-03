2d35cc5365016702551467fee424a68410eeace7
joseeden <josemanuelitoeden@gmail.com>
2025-02-04 07:58:45 +0800

    center jsutified

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,18 +85,18 @@ const config: Config = {
       },
       items: [
         // { to: "/about", label: "About", position: "left" },
-        { to: "/", label: "Home", position: "left" },
+        { to: "/", label: "Home", position: "center" },
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
           label: 'Documentation',
         },        
-        { to: "/blog", label: "Blog", position: "left" },
-        { to: "/", label: "Running", position: "left" },
+        { to: "/blog", label: "Blog", position: "center" },
+        { to: "/", label: "Running", position: "center" },
         // { to: "/", label: "Gallery", position: "left" },
         // { to: "/", label: "Arts", position: "left" },
-        { to: "https://merria.co", label: "Shop", position: "left" },
+        { to: "https://merria.co", label: "Shop", position: "center" },
         // {
         //   href: 'https://github.com/joseeden',
         //   label: 'GitHub',
