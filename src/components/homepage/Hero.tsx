082a40de415d5ca2ba2c9cb375c2dc7e1b8d1519
joseeden <josemanuelitoeden@gmail.com>
2025-02-04 11:12:56 +0800

    Added photo to homepage

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -7,15 +7,20 @@ import styles from "./Hero.module.scss";
 export const Hero: FunctionComponent = () => {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className={clsx("container", "homepage")}>
-        <h1 className={clsx("hero__title", styles.title)}>
-          Hi. I'm <span className={styles.highlighted}>Eden</span>.
-        </h1>
-        <p className={clsx("hero__subtitle", styles.subtitle)}>
-          I'm a problem solver who enjoys learning and solving challenges.
-          <br />
-          An engineer by day, runner by night.
-        </p>
+      <div className={clsx("container", "homepage", styles.heroContainer)}>
+        <div className={styles.textContainer}>
+          <h1 className={clsx("hero__title", styles.title)}>
+            Hi. I'm <span className={styles.highlighted}>Eden</span>.
+          </h1>
+          <p className={clsx("hero__subtitle", styles.subtitle)}>
+            I'm a problem solver who enjoys learning and solving challenges.
+            <br />
+            An engineer by day, runner by night.
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src="img/about/me-3.jpg" alt="Eden Jose" className={styles.heroImage} />
+        </div>
       </div>
     </header>
   );
