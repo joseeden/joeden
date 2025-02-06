2d9c84c3316fcb358588e4f1876b7da0ba0ceb14
joseeden <josemanuelitoeden@gmail.com>
2025-02-07 01:49:35 +0800

    Added construction page

---
+++ src/components/error404/ongoingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ongoing.scss';
+
+const OngoingPage: React.FC = () => {
+  return (
+    <div className="ongoing-container">
+      <img src="./ongoing.png" alt="Ongoing" className="ongoing-image" />
+    </div>
+  );
+};
+
+export default OngoingPage;
