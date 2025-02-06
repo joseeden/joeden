5711de5e0f6b55a0080ba584a28eff1f9af02ccc
joseeden <josemanuelitoeden@gmail.com>
2025-02-07 03:18:01 +0800

    Updated 404 file

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '/ongoing/ongoing.scss';
 
-const Error404Page: React.FC = () => {
-  return (
-    <div className="ongoing-container">
-      <img src={require('/ongoing/ongoing.png').default} alt="Ongoing" className="ongoing-image" />
-    </div>
-  );
-};
+const Error404Page = () => {
+    return (
+      <div className="ongoing-container">
+        <img src="/img/error/ongoing.png" alt="Ongoing" className="ongoing-image" />
+      </div>
+    );
+  };
 
 export default Error404Page;
