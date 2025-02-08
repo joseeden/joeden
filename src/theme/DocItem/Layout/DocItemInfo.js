27e440b3262234e62f9c0e4304ffd57464a26a5b
joseeden <josemanuelitoeden@gmail.com>
2025-02-09 02:05:29 +0800

    Using old package

--- src/theme/DocItem/Layout/DocItemInfo.js
+++ src/theme/DocItem/Layout/DocItemInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReadingTime from '../../../components/documentation/ReadingTime';
 // import ShareButton from '../ShareButton';
-// import { useDoc } from '@docusaurus/theme-common/internal';
-import { useDoc } from '@docusaurus/theme-common'; 
+import { useDoc } from '@docusaurus/theme-common/internal';
+// import { useDoc } from '@docusaurus/theme-common'; 
 
 function DocItemInfo() {
   const { metadata } = useDoc();
--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
 import { useWindowSize } from '@docusaurus/theme-common';
-// import { useDoc } from '@docusaurus/theme-common/internal';
-import { useDoc } from '@docusaurus/theme-common'; 
+import { useDoc } from '@docusaurus/theme-common/internal';
+// import { useDoc } from '@docusaurus/theme-common'; 
 import DocItemPaginator from '@theme/DocItem/Paginator';
 import DocVersionBanner from '@theme/DocVersionBanner';
 import DocVersionBadge from '@theme/DocVersionBadge';
