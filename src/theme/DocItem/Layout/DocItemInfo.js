46f7114c42d7020b39e78818f9d41eea07ed43cb
joseeden <josemanuelitoeden@gmail.com>
2025-02-09 02:12:00 +0800

    Fix 'useDoc' issue. Blog-related @docusaurus/theme-common/internal APIs have been moved to @docusaurus/plugin-content-blog/client. Reference: https://github.com/facebook/docusaurus/issues/10404

--- src/theme/DocItem/Layout/DocItemInfo.js
+++ src/theme/DocItem/Layout/DocItemInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReadingTime from '../../../components/documentation/ReadingTime';
 // import ShareButton from '../ShareButton';
-import { useDoc } from '@docusaurus/theme-common/internal';
-// import { useDoc } from '@docusaurus/theme-common'; 
+// import { useDoc } from '@docusaurus/theme-common/internal';
+import {useDoc} from '@docusaurus/plugin-content-docs/client';     /* Reference: https://github.com/facebook/docusaurus/issues/10404 */
 
 function DocItemInfo() {
   const { metadata } = useDoc();
