6af3279af50d6a85f39106474d9594b4a095de6a
joseeden <josemanuelitoeden@gmail.com>
2025-02-10 20:40:27 +0800

    Moved files, added notebook, updated notes

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import styles from "../components/projects/Project.module.scss";
+import styles from ".../components/projects/Project.module.scss";
 
 import { Project, ProjectData } from "../components/projects/Project";
 
 const assetsDir = "./assets/projects";
 const projects: ProjectData[] = [
+  {
+    title: "A Network Analysis of Game of Thrones",
+    description: "Using Jupyter Notebooks, analyze the co-occurrence network of the characters in the Game of Thrones books",
+    url: "https://colab.research.google.com/github/joseeden/joeden/blob/master/docs/021-Software-Engineering/021-Jupyter-Notebooks/020-A-Network-Analysis-of-Game-of-Thrones/network-analysis-on-game-of-thrones.ipynb",
+    image: require(`${assetsDir}/got_network.jpeg`),
+    // role: "Organizer",
+  },
   {
     title: "Centralized Logging with Graylog SIEM",
     description:
@@ -40,6 +47,13 @@ const projects: ProjectData[] = [
     description: "Develop an ETL pipeline to extract, transform, and load large datasets.",
     url: "",
     image: require(`${assetsDir}/etl-pipeline.png`),
+    // role: "Organizer",
+  },
+  {
+    title: "Investigating Netflix Movies",
+    description: "Using Jupyter Notebooks, perform an exploratory data analysis on movies released in the 1990s",
+    url: "https://colab.research.google.com/github/joseeden/joeden/blob/master/docs/021-Software-Engineering/021-Jupyter-Notebooks/010-Investigating-Netflix-Movies/netflix-movies.ipynb",
+    image: require(`${assetsDir}/redpopcorn.jpg`),
     // role: "Organizer",
   },
 ];
