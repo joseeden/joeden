9dc3e9b4e8f1fc8b06826d625b1d16f616b316fc
joseeden <josemanuelitoeden@gmail.com>
2025-02-11 22:43:46 +0800

    Added update timestamp to readingtime line

--- src/components/documentation/ReadingTime.js
+++ src/components/documentation/ReadingTime.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import readingTime from 'reading-time/lib/reading-time';
 
-function ReadingTime(props) {
+function ReadingTime({ lastUpdated }) {
   const [readingTimeInWords, setReadingTimeInWords] = useState('');
 
   useEffect(() => {
     setReadingTimeInWords(
       readingTime(document.querySelector('.markdown').innerText).text,
     );
-  });
+  }, []);
+
+  const formattedDate = lastUpdated
+    ? lastUpdated.toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' })
+    : '';
+
   return (
     <div style={{ marginBottom: '0.5rem' }}>
-      {/* <span className="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-small text-gray-800"> */}
-      <span className="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-small" style={{ color: '#757575', fontSize: '0.75rem', backgroundColor: 'transparent' }}> 
-        {readingTimeInWords}
+      <span 
+        className="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-small" 
+        style={{ color: '#757575', fontSize: '0.75rem', backgroundColor: 'transparent' }}
+      >
+        {formattedDate && `Updated ${formattedDate} - `}{readingTimeInWords}
       </span>
     </div>
   );
--- src/theme/DocItem/Layout/DocItemInfo.js
+++ src/theme/DocItem/Layout/DocItemInfo.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReadingTime from '../../../components/documentation/ReadingTime';
-// import ShareButton from '../ShareButton';
-// import { useDoc } from '@docusaurus/theme-common/internal';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';     /* Reference: https://github.com/facebook/docusaurus/issues/10404 */
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
 
 function DocItemInfo() {
   const { metadata } = useDoc();
+  const lastUpdated = metadata.lastUpdatedAt ? new Date(metadata.lastUpdatedAt * 1000) : null;
+
   return (
     <div className="flex justify-between">
-      <ReadingTime />
-      {/* <ShareButton title={metadata.title} /> */}
+      <ReadingTime lastUpdated={lastUpdated} />
     </div>
   );
 }
