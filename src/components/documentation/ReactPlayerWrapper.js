b6ba4b265b1dc8cce3d5c419d2574feab25f11b8
joseeden <josemanuelitoeden@gmail.com>
2025-03-13 23:18:25 +0800

    Reverted to previous version without pinch-to-zoom

--- src/components/documentation/ReactPlayerWrapper.js
+++ src/components/documentation/ReactPlayerWrapper.js
@@ -1,23 +1,13 @@
 // src/components/documentation/ReactPlayerWrapper.js
 import React from 'react';
 import ReactPlayer from 'react-player';
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const ReactPlayerWrapper = (props) => {
-  return (
-    <div className="video-container">
-      <TransformWrapper
-        wheel={{ step: 0.1 }} // Mouse scroll zoom
-        pinch={{ step: 0.1 }} // Pinch-to-zoom gesture on mobile
-        minScale={0.5} // Minimum zoom out
-        maxScale={3} // Maximum zoom in
-      >
-        <TransformComponent>
-          <ReactPlayer {...props} />
-        </TransformComponent>
-      </TransformWrapper>
-    </div>
-  );
+    return (
+        <div className="video-container">
+            <ReactPlayer {...props} />
+        </div>
+    );
 };
 
 export default ReactPlayerWrapper;
