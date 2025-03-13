3b9085376536e8ebbf22a45d61616878cb516213
joseeden <josemanuelitoeden@gmail.com>
2025-03-13 22:49:27 +0800

    Update

--- src/components/documentation/ReactPlayerWrapper.js
+++ src/components/documentation/ReactPlayerWrapper.js
@@ -1,13 +1,24 @@
 // src/components/documentation/ReactPlayerWrapper.js
 import React from 'react';
 import ReactPlayer from 'react-player';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const ReactPlayerWrapper = (props) => {
-    return (
-        <div className="video-container">
-            <ReactPlayer {...props} />
-        </div>
-    );
+  return (
+    <div className="video-container">
+      <TransformWrapper
+        defaultScale={1}
+        wheel={{ step: 0.1 }}
+        pinch={{ step: 0.1 }}
+        minScale={0.5}
+        maxScale={3}
+      >
+        <TransformComponent>
+          <ReactPlayer {...props} />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
 };
 
 export default ReactPlayerWrapper;
