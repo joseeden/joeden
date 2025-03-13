ceead0011bfadb3f3e2a55dc4b18be8545f544f8
joseeden <josemanuelitoeden@gmail.com>
2025-03-13 23:33:42 +0800

    Revert "Reverted to previous version without pinch-to-zoom"
    This reverts commit b6ba4b265b1dc8cce3d5c419d2574feab25f11b8.

--- src/components/documentation/ReactPlayerWrapper.js
+++ src/components/documentation/ReactPlayerWrapper.js
@@ -1,13 +1,23 @@
 // src/components/documentation/ReactPlayerWrapper.js
 import React from 'react';
 import ReactPlayer from 'react-player';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const ReactPlayerWrapper = (props) => {
-    return (
-        <div className="video-container">
-            <ReactPlayer {...props} />
-        </div>
-    );
+  return (
+    <div className="video-container">
+      <TransformWrapper
+        wheel={{ step: 0.1 }} // Mouse scroll zoom
+        pinch={{ step: 0.1 }} // Pinch-to-zoom gesture on mobile
+        minScale={0.5} // Minimum zoom out
+        maxScale={3} // Maximum zoom in
+      >
+        <TransformComponent>
+          <ReactPlayer {...props} />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
 };
 
 export default ReactPlayerWrapper;
