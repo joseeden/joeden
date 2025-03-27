44641611495691b2adaadcd6243212621b57b409
joseeden <josemanuelitoeden@gmail.com>
2025-03-27 21:19:03 +0800

    Update notes

--- docs/021-Software-Engineering/009-Web-Development/Projects/014-Circuit-Minds/script.js
+++ docs/021-Software-Engineering/009-Web-Development/Projects/014-Circuit-Minds/script.js
@@ -3,8 +3,25 @@ if (window.location.hash) {
   history.replaceState(null, null, ' ');
 }
 
-// Enable scrolling only when clicking a navbar link
+// Enable scrolling only when clicking a navbar link or button
 function scrollToSection(event, sectionId) {
   event.preventDefault();
-  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+  const target = document.getElementById(sectionId);
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
 }
+
+// Mailchimp form setup (to handle form data)
+(function($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
+})(jQuery);
+
+var $mcj = jQuery.noConflict(true);
