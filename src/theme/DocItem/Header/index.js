737a6152b5c424f1540a487e31a7fb3f28d8a1b2
joseeden <josemanuelitoeden@gmail.com>
2026-01-12 04:35:54 +0800

    Feat: Adding debug to check if header is working

--- src/theme/DocItem/Header/index.js
+++ src/theme/DocItem/Header/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import Header from '@theme-original/DocItem/Header';
-import {useDoc} from '@docusaurus/theme-common';
-import TagsListInline from '@theme/TagsListInline';
 
 export default function HeaderWrapper(props) {
-  const {metadata} = useDoc();
-  const tags = metadata?.tags || [];
-  
   return (
     <>
       <Header {...props} />
-      {tags.length > 0 && (
-        <div className="custom-tags-header">
-          <TagsListInline tags={tags} />
+      <div className="custom-tags-header">
+        <div style={{background: 'red', padding: '10px', color: 'white'}}>
+          DEBUG: Custom header component is working!
         </div>
-      )}
+      </div>
     </>
   );
 }
