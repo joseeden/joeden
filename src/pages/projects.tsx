b4ee0e655e3c3a891dc2799914b5a73333e43400
joseeden <josemanuelitoeden@gmail.com>
2026-04-07 20:44:12 +0800

    Update card styling and descriptions

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,7 +17,7 @@ const projects: ProjectData[] = [
   },  
   {
     title: "Circuit Minds",
-    description: "Landing page for a fictional electronics engineering company",
+    description: "Landing page for a fictional electronics company",
     url: `${baseUrl}/docs/Software-Engineering/Web-Development/Projects/Circuit-Minds/`, 
     image: require(`/img/docs/Screenshot-2025-03-28-195824.png`),
     tags: ["Web Development", "JavaScript"],
@@ -33,7 +33,7 @@ const projects: ProjectData[] = [
   },  
   {
     title: "CI/CD Pipeline for Flask Application",
-    description: "Automate code deployment with testing, packaging, and server deployment.",
+    description: "Automated code testing, packaging, and deployment.",
     url: `${baseUrl}/docs/Version-Control-and-CICD/Jenkins-Labs/Single-Server-Deployment`, 
     image: require(`/img/docs/jenkins-flask.png`),
     tags: ["DevOps", "Containers", "Python"],
@@ -57,7 +57,7 @@ const projects: ProjectData[] = [
   },  
   {
     title: "Kong Gateway Setup with Docker Compose",
-    description: "Setup Kong API Gateway along with ELK, Prometheus, Grafana, and other tools.",
+    description: "Setup Kong API Gateway with ELK, Prometheus, Grafana, and more.",
     url: `${baseUrl}/docs/Software-Engineering/Kong-API-Gateway/Containerized-Kong-and-Other-Apps`, 
     image: require(`/img/docs/kong-elk-prom-graf.png`),
     tags: ["DevOps", "Docker", "Observability"],
@@ -66,6 +66,7 @@ const projects: ProjectData[] = [
   {
     title: "A Network Analysis of Game of Thrones",
     description: "Analyze co-occurrence network of the characters in the Game of Thrones books",
+    description: "Analyze co-occurrence networks of characters in Game of Thrones",
     url: "https://colab.research.google.com/github/joseeden/joeden/blob/master/docs/065-Software-Engineering/021-Jupyter-Notebooks/020-A-Network-Analysis-of-Game-of-Thrones/network-analysis-on-game-of-thrones.ipynb",
     image: require(`/img/docs/got_network.jpeg`),
     tags: ["Data Analysis", "Python"]
@@ -74,6 +75,7 @@ const projects: ProjectData[] = [
   {
     title: "A New Era of Data Analysis in Baseball",
     description: "Use MLB's Statcast data to compare the home runs of two of baseball's brightest stars.",
+    description: "Use Statcast data to compare home runs of two of MLB's brightest stars.",
     url: "https://colab.research.google.com/github/joseeden/joeden/blob/master/docs/065-Software-Engineering/021-Jupyter-Notebooks/021-A-New-Era-of-Data-Analysis-in-Baseball/A-New-Era-of-Data-Analysis-in-Baseball.ipynb",
     image: require(`/img/docs/stanton_wide.jpg`),
     tags: ["Data Analysis", "Python"]
@@ -81,7 +83,7 @@ const projects: ProjectData[] = [
   },
   {
     title: "Investigating Netflix Movies",
-    description: "Perform an exploratory data analysis on 90s movies using Netflix data.",
+    description: "Exploratory data analysis on 90s movies using Netflix data.",
     url: "https://colab.research.google.com/github/joseeden/joeden/blob/master/docs/065-Software-Engineering/021-Jupyter-Notebooks/010-Investigating-Netflix-Movies/netflix-movies.ipynb",
     image: require(`/img/docs/redpopcorn.jpg`),
     tags: ["Data Analysis", "Python"]
@@ -105,8 +107,7 @@ const projects: ProjectData[] = [
   },  
   {
     title: "Centralized Logging with Graylog SIEM",
-    description:
-      "Deploy Graylog as an SIEM solution to collect logs from AWS resources.",
+    description: "Deploy Graylog as an SIEM solution to collect logs from AWS resources.",
     url: "",
     image: require(`/img/docs/graylog.png`),
     tags: ["DevOps", "Security", "AWS"],
@@ -122,7 +123,7 @@ const projects: ProjectData[] = [
   },
   {
     title: "End-to-End DevSecOps Pipeline",
-    description: "CI/CD pipeline with security scanning, automated testing, and compliance checks.",
+    description: "CICD pipeline with automated testing, security scans, and compliance checks.",
     url: "",
     image: require(`/img/docs/devsecops.png`),
     tags: ["DevOps", "Security"],
@@ -138,7 +139,7 @@ const projects: ProjectData[] = [
   },
   {
     title: "ETL Pipeline Deployment",
-    description: "Develop an ETL pipeline for extracting, transforming, and loading large datasets.",
+    description: "Develop an ETL pipeline to extract, transform, and load large datasets.",
     url: "",
     image: require(`/img/docs/etl-pipeline.png`),
     tags: ["Data Engineering", "ETL", "Python"],
