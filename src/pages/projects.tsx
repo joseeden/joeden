ffd504d13975094c8e6f491a72889c00d2c42e1f
joseeden <josemanuelitoeden@gmail.com>
2026-04-12 06:55:11 +0800

    Updated TSX to keep Reset button always visible.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -213,14 +213,13 @@ export default function Projects(): JSX.Element {
                       </div>
                     ))}
                   </div>
-                  {selectedCategories.length > 0 && (
-                    <button
-                      className={styles.clearAllButton}
-                      onClick={() => setSelectedCategories([])}
-                    >
-                      Clear All
-                    </button>
-                  )}
+                  <button
+                    className={styles.clearAllButton}
+                    onClick={() => setSelectedCategories([])}
+                    disabled={selectedCategories.length === 0}
+                  >
+                    Clear All
+                  </button>
                 </div>
               </div>
               <div className={styles.rightColumn}>
