d74160c3785b6c598a89fcf431de7cd91e888224
joseeden <josemanuelitoeden@gmail.com>
2026-04-12 02:44:04 +0800

    Reduce fontsize of docs titles, including generated category pages

--- declaration.d.ts
+++ declaration.d.ts
@@ -3,6 +3,35 @@ declare module "*.scss" {
   export default content;
 }
 
+declare module "@theme/IdealImage" {
+  import type { ComponentProps, ReactNode } from "react";
+
+  export type IdealImageSource =
+    | string
+    | { default: string }
+    | {
+        preSrc: string;
+        src: {
+          height?: number;
+          width?: number;
+          preSrc: string;
+          src: string;
+          images: Array<{
+            width: number;
+            path?: string;
+            size?: number;
+            format?: "webp" | "jpeg" | "png" | "gif";
+          }>;
+        };
+      };
+
+  export interface Props extends ComponentProps<"img"> {
+    readonly img: IdealImageSource;
+  }
+
+  export default function IdealImage(props: Props): ReactNode;
+}
+
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.md";
