72fd43eedb2f07e86e94bfc3857da13941f2344f
joseeden <josemanuelitoeden@gmail.com>
2026-04-16 16:27:54 +0800

    Update Hero text

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -15,10 +15,7 @@ export const Hero: FunctionComponent = () => {
           
           <div className={styles.narrative}>
             <p>
-              I'm interested in building things and solving problems. I love breaking things down, understanding how they work, and figuring out how to make them better.
-           </p>
-            <p>
-              I write code, work with cloud-native environments, and build systems that are reliable, scalable, and built to handle real-world use. When I'm not coding, you'll find me running or reading books that take me to other worlds.
+              I'm interested in building things and solving problems. I write code, work with cloud-native environments, and build systems that are reliable, scalable, and built to handle real-world use. When I'm not coding, you'll find me running or reading books that take me to other worlds.
             </p>
             <p>
               A big part of my work is around automation, integration, and building tools that glue different things together. I often work with APIs, infrastructure, and open-source tools to reduce manual effort and make things work more seamlessly in production. I work on connecting different pieces and turning ideas into practical solutions.
@@ -32,10 +29,10 @@ export const Hero: FunctionComponent = () => {
             <h2 className={styles.interestsTitle}>INTERESTS</h2>
             <div className={styles.interestsContent}>
               <p>
-                Outside of code, I spend most of my free time running long distances. It helps me clear my head and stay focused. I’ve done multiple half marathons and local runs, where I can just stay in the moment and enjoy the experience. I also run a small home lab where I experiment with servers, networking, and electronics.
+                Outside of code, I spend most of my free time running long distances. It helps me clear my head and stay focused. I’ve done multiple half marathons and local runs, where I can just stay in the moment and get lost in the experience. I also run a small home lab where I experiment with servers, networking, and electronics.
               </p>
               <p>
-                I also spend a lot of time reading. I'm drawn to books that explore complex ideas. I liked Andy Weir's <em>The Martian</em> where the protagonist's resourcefulness and problem-solving approach really resonated with me. I loved Haruki Murakami's surreal views on reality and consciousness, and everything in between. 
+                I also read a lot, especially books that explore complex ideas. I like Andy Weir's <em>The Martian</em> where the protagonist's resourcefulness and problem-solving approach really resonated with me. I love Haruki Murakami's surreal views on reality and consciousness, and everything in between. 
               </p>
               <p>
                 Whether it's hard sci-fi, philosophy, or genre fiction, if it challenges how I think, I'm reading it.
